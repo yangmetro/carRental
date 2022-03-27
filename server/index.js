3d6da20e5123b9d2eb60595f0096074dcdb4630a
Colin Cross <crosstco@gmail.com>
2022-03-27 12:53:06 -0400

    Move db connection to its own file

--- server/index.js
+++ server/index.js
@@ -1,55 +1,51 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const cors = require('cors');
+const mysql = require("mysql");
+const cors = require("cors");
+
+const db = require("./config/db");
+
+// Connect to db
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Database connected.");
+});
 
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-    host: 'carrental.ccvg8mvh0pfx.us-east-2.rds.amazonaws.com',
-    port: '3306',
-    database: 'carRental',
-    user: 'admin',
-    password: 'cs348carRental',
+app.get("/display", (req, res) => {
+  db.query("SELECT * FROM Vehicles", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
+app.post("/addvehicle", (req, res) => {
+  const owner_id = req.body.owner_id;
+  const model = req.body.model;
+  const licensePlate = req.body.licensePlate;
+  const state = req.body.state;
+  const mileage = req.body.mileage;
+  const dailyPrice = req.body.dailyPrice;
+  const location = req.body.location;
 
-app.get('/display', (req, res) => {
-    db.query(
-        "SELECT * FROM Vehicles",
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send(result);
-            }
-        }
-    );
+  db.query(
+    "INSERT INTO vehicles (owner_id, model, licensePlate, state, mileage, dailyPrice, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+    [owner_id, model, licensePlate, state, mileage, dailyPrice, location],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
 });
-
-app.post('/addvehicle', (req, res) => {
-    const owner_id = req.body.owner_id;
-    const model = req.body.model;
-    const licensePlate = req.body.licensePlate;
-    const state = req.body.state;
-    const mileage = req.body.mileage;
-    const dailyPrice = req.body.dailyPrice;
-    const location = req.body.location;
-
-    db.query(
-        "INSERT INTO vehicles (owner_id, model, licensePlate, state, mileage, dailyPrice, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
-        [owner_id, model, licensePlate, state, mileage, dailyPrice, location],
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send(result);
-            }
-        } 
-    )
-})
 
 app.listen(3001, () => {
-    console.log("Server is running");
+  console.log("Server is running");
 });
