4524eeb97cd0c9c994f289779c8f054d4485b8f8
Colin Cross <crosstco@gmail.com>
2022-03-27 12:57:31 -0400

    Authentication middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+// DEVELOPMENT ONLY. Move to environment variable later.
+const jwtSecret = "secret";
+
+module.exports = function (req, res, next) {
+  // Get token from request header
+  const token = req.header("x-auth-token");
+
+  // Check if token exists
+  if (!token) {
+    return res.status(401).json({ msg: "No token, unauthorized" });
+  }
+
+  // Decode the token and get the user
+  try {
+    const decoded = jwt.verify(token, secret);
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Invalid token" });
+  }
+};
