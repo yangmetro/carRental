1bf79429f99a286e2d132ecb56278a30db312522
willkao <willkao21@gmail.com>
2022-04-28 13:01:15 -0400

    Display currently rented cars for user

--- app/src/Components/ReturnVehicle/ReturnVehicle.js
+++ app/src/Components/ReturnVehicle/ReturnVehicle.js
@@ -1,34 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './ReturnVehicle.css';
 import {useState} from 'react';
 import Axios from "axios";
 
 function ReturnVehicle() {
-    const [licensePlate, setPlate] = useState('');
-    const [owner_id, setOwner] = useState(123);
-
-    const handlePlate = event => setPlate(event.target.value);
-
-    const removeVehicle = () => {
-        Axios.post('http://localhost:3001/removevehicle', {
-            owner_id:owner_id,
-            licensePlate:licensePlate
+    const [rentedCars, setRentedCars] = useState([]);
+    const [user_id, setUserId] = useState(136);
+    
+    const getRented = () => {
+        Axios.get('http://localhost:3001/displayRented', {
+            user_id:user_id
         }).then((response) => {
-            console.log(response);
+            if (response.data.length === 0) {
+                console.log("Response is undefined");
+                console.log(response.data);
+            } else {
+                setRentedCars(response.data);
+            }
         });
     };
 
+    useEffect(() => {
+        console.log(user_id);
+        Axios.post('http://localhost:3001/displayRented', {
+            user_id:user_id
+        }).then((response) => {
+            if (response.data.length === 0) {
+                console.log("Response is undefined");
+                console.log(response.data);
+            } else {
+                setRentedCars(response.data);
+            }
+        });
+    }, []);
+
     return (
         <div>
-            <input
-                className='input'
-                name='licensePlate'
-                type='text'
-                placeholder='Enter your license plate number'
-                onChange={handlePlate}
-            />
+            <h1>Return Vehicle</h1>
             <div>
-                <button className='submitButton' onClick={removeVehicle}>Remove</button>
+                {rentedCars.map((val, key) => {
+                    return (
+                        <div key={val.rental_id}>
+                            <h1>{val.license_plate + " " + val.start_date}</h1>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     )
